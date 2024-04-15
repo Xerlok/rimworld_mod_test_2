4cdba164a68938fbf38120faf9b789c11965c973
Xerlok <20791727+Xerlok@users.noreply.github.com>
2024-04-15 17:52:36 +0300

    Change XML to Wormax and create DLL

---
+++ Source/rimworld-mod-test/rimworld-mod-test/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace Wormax
+{
+    public class IngestionOutcomeDoer_RemoveHediff : IngestionOutcomeDoer
+    {
+        public HediffDef removeThisThing;
+
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+            throw new NotImplementedException();
+            var gutwormsHediff = pawn.health.hediffSet.GetFirstHediffOfDef(removeThisThing);
+            if (gutwormsHediff != null)
+            {
+                pawn.health.hediffSet.hediffs.Remove(gutwormsHediff);
+            }
+        }
+    }
+}
